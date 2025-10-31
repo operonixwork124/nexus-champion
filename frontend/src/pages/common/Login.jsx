7862aca4ef36bc1d6c08e18e132c28e136815cef
richard <richard@out.com>
2025-11-01 03:29:15 +0900

    init

--- frontend/src/pages/common/Login.jsx
+++ frontend/src/pages/common/Login.jsx
@@ -6,12 +6,6 @@ const Login = () => {
       <section id="login-section">
         <div className="py-40 pt-36 xl:pb-[200px] xl:pt-[180px]">
           <div className="global-container text-center">
-            <h1 className="mb-[50px]">Welcome Back</h1>
-            <img
-              src="/task"
-              alt="Login illustration"
-              className="mx-auto max-w-[400px] rounded-lg shadow-lg"
-            />
           </div>
         </div>
       </section>
